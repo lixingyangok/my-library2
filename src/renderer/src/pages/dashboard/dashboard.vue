e25ccc075fd65bda2928e16de2d8e5a931e29368
lixingyangok <176840078@qq.com>
2023-05-12 19:57:22 +0700

    新建看板页

---
+++ src/renderer/src/pages/dashboard/dashboard.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: 李星阳
+ * @Date: 2021-12-05 17:59:27
+ * @LastEditors: 李星阳
+ * @LastEditTime: 2023-05-12 19:56:06
+ * @Description: 
+-->
+
+<template>
+    <h1>
+        dashboard
+    </h1>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            
+        };
+    },
+};
+</script>
+
+<style scoped lang="scss" src="./style/dashboard.scss">
+
+</style>
+
