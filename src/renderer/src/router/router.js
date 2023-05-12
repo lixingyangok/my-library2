e9e55f8e73b0eb130486ff503c107c6a9048f6bb
lixingyangok <176840078@qq.com>
2023-05-12 19:58:10 +0700

    导航添加新页面

--- src/renderer/src/router/router.js
+++ src/renderer/src/router/router.js
@@ -23,56 +23,63 @@ export const aTools = [{
 }];
 
 
-export const routes = [
-    {
-        path: '/',
-        name_: '首页',
-        icon_: 'fas fa-desktop',
-        // icon_: 'fas fa-poll',
-        component: ()=>import( /* webpackChunkName: "welcome.vue" */
-            '../pages/welcome/welcome.vue'
-        ),
-    },{
-        path: '/shelf',
-        name_: '书架',
-        icon_: 'fas fa-folder-open',
-        component: ()=>import( /* webpackChunkName: "shelf.vue" */ 
-            '../pages/shelf/shelf.vue'
-        ),
-    },{
-        path: '/study-lounge',
-        name: 'studyLounge',
-        name_: '听写',
-        // icon_: 'fas fa-keyboard',
-        icon_: 'fas fa-graduation-cap',
-        component: ()=>import( /* webpackChunkName: "study-lounge.vue" */ 
-            '../pages/study-lounge/study-lounge.vue'
-        ),
-    },{
-        path: '/settings',
-        name_: '设置',
-        icon_: 'fas fa-cog',
-        component: ()=>import( /* webpackChunkName: "settings.vue" */
-            '../pages/settings/settings.vue'
-        ),
-    },{
-        path: '/dictionary',
-        name_: '字典',
-        icon_: 'fas fa-book',
-        component: ()=>import( /* webpackChunkName: "dictionary.vue" */
-            '../pages/dictionary/dictionary.vue'
-        ),
-    },{
-        path: '/tools',
-        redirect: '/tools/alarm-clock',
-        name_: '工具',
-        icon_: 'fas fa-grip-horizontal',
-        component: ()=>import( /* webpackChunkName: "tool-portal.vue" */
-            '../pages/tools/tool-portal/tool-portal.vue'
-        ),
-        children: aTools,
-    },
-];
+export const routes = [ {
+    path: '/',
+    name_: '首页',
+    icon_: 'fas fa-desktop',
+    // icon_: 'fas fa-poll',
+    component: ()=>import( /* webpackChunkName: "welcome.vue" */
+        '../pages/welcome/welcome.vue'
+    ),
+},{
+    path: '/shelf',
+    name_: '书架',
+    icon_: 'fas fa-folder-open',
+    component: ()=>import( /* webpackChunkName: "shelf.vue" */ 
+        '../pages/shelf/shelf.vue'
+    ),
+},{
+    path: '/study-lounge',
+    name: 'studyLounge',
+    name_: '听写',
+    // icon_: 'fas fa-keyboard',
+    icon_: 'fas fa-graduation-cap',
+    component: ()=>import( /* webpackChunkName: "study-lounge.vue" */ 
+        '../pages/study-lounge/study-lounge.vue'
+    ),
+},{
+    path: '/dashboard',
+    name: 'dashboard',
+    name_: '统计',
+    // icon_: 'fas fa-keyboard',
+    icon_: 'fas fa-graduation-cap',
+    component: ()=>import( /* webpackChunkName: "dashboard.vue" */ 
+        '../pages/dashboard/dashboard.vue'
+    ),
+},{
+    path: '/settings',
+    name_: '设置',
+    icon_: 'fas fa-cog',
+    component: ()=>import( /* webpackChunkName: "settings.vue" */
+        '../pages/settings/settings.vue'
+    ),
+},{
+    path: '/dictionary',
+    name_: '字典',
+    icon_: 'fas fa-book',
+    component: ()=>import( /* webpackChunkName: "dictionary.vue" */
+        '../pages/dictionary/dictionary.vue'
+    ),
+},{
+    path: '/tools',
+    redirect: '/tools/alarm-clock',
+    name_: '工具',
+    icon_: 'fas fa-grip-horizontal',
+    component: ()=>import( /* webpackChunkName: "tool-portal.vue" */
+        '../pages/tools/tool-portal/tool-portal.vue'
+    ),
+    children: aTools,
+}, ];
 
 const router = createRouter({
     history: createWebHashHistory(),
