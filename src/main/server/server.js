e625b715140696329117b305c840d41594244a9c
lixingyangok <176840078@qq.com>
2023-09-09 20:23:53 +0700

    初步完成接口基础

--- src/main/server/server.js
+++ src/main/server/server.js
@@ -10,77 +10,61 @@ const oApiFn = {
 
 // 开启局域网接口
 function openServer() {
-    // 防止重复开启
-    closeServer();
-    // 获取本机的局域网IP和自定义端口
+    closeServer(); // 防止重复开启
+    let SERVER_IP = 'localhost'; // 127.0.0.1
     let SERVER_PORT = 8899;
-    // let SERVER_IP = getServerIp();
-    // let SERVER_IP = '127.0.0.1';
-    let SERVER_IP = 'localhost';
+    const sRootURL = `http://${SERVER_IP}:${SERVER_PORT}`;
     server = http.createServer();
-    // server = http.createServer(function (request, response) {
-    //     // 回调函数接收request和response对象,
-    //     // 获得HTTP请求的method和url:
-    //     console.log(request.method + ': ' + request.url);
-    //     // 将HTTP响应200写入response, 同时设置Content-Type: text/html:
-    //     // response.writeHead(200, {"Content-Type": "application/json;charset=utf-8", "access-control-allow-origin": "*",});
-    //     response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
-    //     // 将HTTP响应的HTML内容写入response:
-    //     response.end(JSON.stringify({abc: 'Hello world!' + new Date()*1}));
-    // });
     server.on('request', (req, res) => {
-        // 防止跨域
         const {method, url} = req;
+        const oRequestUrl = urlToPlainObject(new URL(url, sRootURL));
         let iAskMark = url.indexOf('?');
         let iEnd = iAskMark > -1 ? iAskMark : url.length;
         const sUrl = url.slice(0, iEnd);
         const fnDoer = oApiFn[sUrl] || (()=>({}));
         res.writeHead(200, {
             "Content-Type": "application/json;charset=utf-8",
-            "access-control-allow-origin": "*",
+            "access-control-allow-origin": "*", // 防止跨域
         });
-        let param = null; // 监听传递的值
+        let body = [];
+        let oResult = {'接口被叫时间': new Date().toString() };
         req.on("data", function (postDataChunk) {
-            param = querystring.parse(postDataChunk.toString());
-            toLog('收到的参数 01', typeof param);
-            toLog('收到的参数 02', Object.keys(param));
-            toLog('收到的参数 03', param);
-            let oResult = {'接口被叫时间': new Date().toString() };
-            Object.assign(oResult, {
-                ...fnDoer(param),
-            });
-            res.end(JSON.stringify(oResult));
+            body.push(postDataChunk);
         });
         // 监听 接口
         req.on('end',()=>{
-            let oResult = {};
-            if (0){ return res.end(JSON.stringify({msg: 'wrong'})); }
-            // if (req.url==)
-            // if (1 || req.method === 'POST' && req.url === '/get_text') { }
-            oResult = {
+            if (body.length){
+                body = Buffer.concat(body).toString();
+                body = JSON.parse(body);
+            }
+            Object.assign(oResult, {
                 code: 200,
-                param,
-                data: {
-                    type: `Hello World! ${new Date()*1}`,
-                    url: req.url,
+                submit: {
+                    body,
+                    oRequestUrl: urlToPlainObject(oRequestUrl),
+                    url,
+                    method,
                     sUrl,
-                    ...fnDoer(),
                 },
-            };
+                data: {
+                    ...fnDoer(body),
+                },
+            });
             // toLog(JSON.stringify(context));
             res.end(JSON.stringify(oResult));
-        })
-    })
+        });
+    });
     // 返回端口开启结果
     return new Promise((resolve, reject) => {
-        // server.listen(8899);
+        // 参数123 = 端口，地址，回调函数
         server.listen(SERVER_PORT, SERVER_IP, () => {
-            // 服务器正确开启
-            resolve({
+            const status = {
                 code: 200,
-                data: `http://${SERVER_IP}:${SERVER_PORT}`,
-                msg: `服务器开启成功，服务器地址: http://${SERVER_IP}:${SERVER_PORT}`
-            })
+                data: sRootURL,
+                msg: `服务器开启成功，地址: ${sRootURL}`,
+            };
+            console.log(status.msg);
+            resolve(status);
         });
         server.on('error', (err) => {
             if (err.code === 'EADDRINUSE') {
@@ -88,8 +72,8 @@ function openServer() {
                 reject({
                     code: 404,
                     data: `端口:${SERVER_PORT}被占用,请更换占用端口`,
-                    msg: `端口:${SERVER_PORT}被占用,请更换占用端口`
-                })
+                    msg: `端口:${SERVER_PORT}被占用,请更换占用端口`,
+                });
             }
         });
     });
@@ -103,27 +87,67 @@ function closeServer() {
     });
 }
 
-// 获取本机的局域网IP
-function getServerIp() {
-    let interfaces = os.networkInterfaces();
-    for (let devName in interfaces) {
-        let iface = interfaces[devName];
-        for (let i = 0; i < iface.length; i++) {
-            let alias = iface[i];
-            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-                return alias.address;
-            }
-        }
-    }
-}
-
 export {
     openServer,
     closeServer
+};
+
+
+function urlToPlainObject(url) {
+    const plainObject = {
+        oParams:{},
+    };
+    for (const key in url) {
+        if (typeof url[key] === 'string') {
+            plainObject[key] = url[key];
+        }
+    }
+    if (url.search){
+        const search = decodeURI(url.search).slice(1);
+        const sParams = `{"${search.replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"')}"}`;
+        plainObject.oParams = JSON.parse(sParams);
+    }
+    return plainObject;
 }
 
-/* 
-作者：雾恋
-链接：https://juejin.cn/post/7220997060136796218
-来源：稀土掘金
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。 */
+
+function copy_it(obj){
+    const oInfo = Object.entries(obj).reduce((oResult, oCur)=>{
+        const [key,val] = oCur;
+        try{
+            let newObj = {...oResult, [key]: val};
+            newObj = JSON.parse(JSON.stringify(newObj));
+            return newObj;
+        }catch(err){
+            oResult[key] = `___${typeof val}`;
+            return oResult;
+        }
+    }, {copied: true});
+    return oInfo;
+}
+
+// server = http.createServer(function (request, response) {
+//     // 回调函数接收request和response对象,
+//     // 获得HTTP请求的method和url:
+//     console.log(request.method + ': ' + request.url);
+//     // 将HTTP响应200写入response, 同时设置Content-Type: text/html:
+//     // response.writeHead(200, {"Content-Type": "application/json;charset=utf-8", "access-control-allow-origin": "*",});
+//     response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+//     // 将HTTP响应的HTML内容写入response:
+//     const oInfo = Object.entries(request).reduce((oResult, oCur)=>{
+//         const [key,val] = oCur;
+//         try{
+//             let newObj = {...oResult, [key]: val};
+//             newObj = JSON.parse(JSON.stringify(newObj));
+//             return newObj;
+//         }catch(err){
+//             oResult[key] = `___${typeof val}`;
+//             return oResult;
+//         }
+//     }, {test: 123});
+    
+//     response.end(JSON.stringify({
+//         timeVal: new Date()*1,
+//         ...oInfo,
+//     }));
+// });
