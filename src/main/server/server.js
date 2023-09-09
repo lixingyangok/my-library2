3ed018b56e2ed3e28614f8302f7d5ece29c64134
lixingyangok <176840078@qq.com>
2023-09-09 21:03:29 +0700

    修改接口

--- src/main/server/server.js
+++ src/main/server/server.js
@@ -1,7 +1,5 @@
 import {get_text, send_text} from './read-aloud.js';
-const os = require('os');
 const http = require('http');
-var querystring = require("querystring");
 let server;
 const oApiFn = {
     '/get_text': get_text,
@@ -11,49 +9,42 @@ const oApiFn = {
 // 开启局域网接口
 function openServer() {
     closeServer(); // 防止重复开启
-    let SERVER_IP = 'localhost'; // 127.0.0.1
-    let SERVER_PORT = 8899;
+    const SERVER_IP = 'localhost'; // 127.0.0.1
+    const SERVER_PORT = 8899;
     const sRootURL = `http://${SERVER_IP}:${SERVER_PORT}`;
-    server = http.createServer();
-    server.on('request', (req, res) => {
-        const {method, url} = req;
-        const oRequestUrl = urlToPlainObject(new URL(url, sRootURL));
-        let iAskMark = url.indexOf('?');
-        let iEnd = iAskMark > -1 ? iAskMark : url.length;
-        const sUrl = url.slice(0, iEnd);
-        const fnDoer = oApiFn[sUrl] || (()=>({}));
+    server = http.createServer((request, res) => {
+        const oRequestUrl = urlToPlainObject(new URL(request.url, sRootURL));
+        const {pathname} = oRequestUrl;
+        const {method} = request;
+        const fnDoer = oApiFn[pathname] || (() => ({}));
         res.writeHead(200, {
             "Content-Type": "application/json;charset=utf-8",
             "access-control-allow-origin": "*", // 防止跨域
         });
-        let body = [];
-        let oResult = {'接口被叫时间': new Date().toString() };
-        req.on("data", function (postDataChunk) {
-            body.push(postDataChunk);
+        let aBody = [];
+        request.on("data", function (postDataChunk) {
+            aBody.push(postDataChunk);
         });
         // 监听 接口
-        req.on('end',()=>{
-            if (body.length){
-                body = Buffer.concat(body).toString();
-                body = JSON.parse(body);
-            }
-            Object.assign(oResult, {
+        request.on('end',()=>{
+            // let oResult = {}; // '接口被叫时间': new Date().toString()
+            const oBody = getBody(aBody);
+            let oResult = {
                 code: 200,
                 submit: {
-                    body,
+                    body: oBody,
                     oRequestUrl: urlToPlainObject(oRequestUrl),
-                    url,
+                    url: request.url,
                     method,
-                    sUrl,
                 },
                 data: {
-                    ...fnDoer(body),
+                    ...fnDoer(oBody),
                 },
-            });
-            // toLog(JSON.stringify(context));
+            };
             res.end(JSON.stringify(oResult));
         });
     });
+    // server.on('request', );
     // 返回端口开启结果
     return new Promise((resolve, reject) => {
         // 参数123 = 端口，地址，回调函数
@@ -91,6 +82,13 @@ export {
     openServer,
     closeServer
 };
+
+function getBody(bodyData){
+    if (!bodyData.length) return {};
+    const sBody = Buffer.concat(bodyData).toString();
+    const oBody = JSON.parse(sBody);
+    return oBody;
+}
 
 
 function urlToPlainObject(url) {
