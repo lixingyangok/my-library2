a14cd1717bdc1d7e69a79466ed94d202311db32d
lixingyangok <176840078@qq.com>
2023-09-10 19:53:49 +0700

    添加新首页

---
+++ src/renderer/src/pages-browser/app/app-browser.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: 李星阳
+ * @Date: 2023-09-10 17:12:37
+ * @LastEditors: 李星阳
+ * @LastEditTime: 2023-09-10 19:24:41
+ * @Description: 
+-->
+<template>
+    <div class="page-body">
+        <h1>
+            <time>{{ sTime }}</time>
+        </h1>
+    </div>
+</template>
+
+<script>
+import all_fn from './js/app-browser.js';
+
+export default {
+    data(){
+        return {
+            sTime: '',
+        };
+    },
+    async created(){
+        this.setTime();
+        setInterval(this.getText, 200);
+
+    },
+    methods:{
+        ...all_fn,
+
+    },
+}
+</script>
+
+<style src="./style/app-browser.css">
+    
+</style>
---
+++ src/renderer/src/pages-browser/app/js/app-browser.js
@@ -0,0 +1,118 @@
+/*
+ * @Author: 李星阳
+ * @Date: 2023-09-10 17:13:28
+ * @LastEditors: 李星阳
+ * @LastEditTime: 2023-09-10 19:51:11
+ * @Description: 
+ */
+
+
+export default {
+    async getText(){
+        const oRes = await fetch('/api/get_text', {
+            method: 'get',
+            headers: { "Content-Type": "application/json"},
+        });
+        const oData = await oRes.json();
+        const {text} = oData.data;
+        if (!text) return;
+        console.log('=>', text);
+        text && toReadAloud(text);
+    },
+    setTime(){
+        const this_ = this;
+        setInterval(()=>{
+            this_.sTime = new Date().toString();
+        }, 1_000);
+    },
+};
+
+// await fetch('/api/send_text', {
+//     method: 'post',
+//     headers: { "Content-Type": "application/json"},
+//     body: JSON.stringify({
+//         sTime: '呼叫接口时间：' + new Date().toString(), 
+//     }),
+// });
+
+var text = '许多人认为当他们富有，取得成功时，幸福自然就会随之而来。我告诉你:事实并非如此';
+var text = 'Many people think that when they become rich and successful, happiness will naturally follow.'; 
+
+function toReadAloud(text){
+    window.speechSynthesis.cancel();
+    var oMsg = Object.assign(new SpeechSynthesisUtterance(), {
+        voice: getVoice(text, 'Jenny'),
+        lang: 'zh-CN', // lang 
+        volume: 100,
+        rate: 1, // 速度
+        pitch: 1, // 值大音尖
+        text,
+    });
+    window.speechSynthesis.speak(oMsg);
+}
+
+function getVoice(sText, sAnnouncer){
+    let aLang = (()=>{
+        var en = ['en-US', 'en-GB']; 
+        var zh = ['zh-CN']; 
+        if (sAnnouncer) return en.concat(zh);
+        const isChinese = /[\u4e00-\u9fa5]/.test(sText);
+        return isChinese ? zh : en;
+    })();
+    const aExclude = ['Yunxia', 'Xiaoyi', 'Ana'];
+    var aAllVoices = window.speechSynthesis.getVoices();
+    var aTargetLang = aAllVoices.filter(cur => aLang.includes(cur.lang));
+    aTargetLang = aTargetLang.filter(cur => !aExclude.includes(cur.name));
+    var aOnlineService = aTargetLang.filter(cur => !cur.localService);
+    var oTarget = (()=>{
+        if (!sAnnouncer) return aOnlineService[0];
+        var regExp = new RegExp(sAnnouncer, 'i');
+        return aOnlineService.find(cur => cur.name.match(regExp));
+    })();
+    var sPrint = aOnlineService.map(cur=>cur.name.replace('Microsoft ', '')).join('\n');
+    console.log(`aOnlineService ■■■■■■■■■■■\n`, sPrint);
+    console.log(`oTarget`, oTarget);
+    return oTarget;
+}
+
+
+
+// window.speechSynthesis.speak(oMsg);
+// window.speechSynthesis.getVoices()
+// window.speechSynthesis.cancel();
+// window.speechSynthesis.pause();
+// window.SpeechSynthesis.resume()
+
+
+
+
+// ▼微软语音
+var aList = [
+    "Microsoft Christopher Online (Natural) - English (United States)", // 男
+    "Microsoft Eric Online (Natural) - English (United States)", // 男
+    "Microsoft Guy Online (Natural) - English (United States)", // 男，感情激昂
+    "Microsoft Roger Online (Natural) - English (United States)", // 男，感情
+    "Microsoft Steffan Online (Natural) - English (United States)", // 男，感情
+    "Microsoft Jenny Online (Natural) - English (United States)", // 女 2021 年，微软推出了 Jenny Multilingual 具备“自动语言预测功能
+    "Microsoft Michelle Online (Natural) - English (United States)", // 女
+    "Microsoft Aria Online (Natural) - English (United States)", // 女
+    // 
+    "Microsoft Yunxi Online (Natural) - Chinese (Mainland)", // 男，常见播音
+    "Microsoft Yunjian Online (Natural) - Chinese (Mainland)", // 男
+    "Microsoft Yunyang Online (Natural) - Chinese (Mainland)", // 男
+    "Microsoft Xiaoxiao Online (Natural) - Chinese (Mainland)", // 女
+    // 
+    "Microsoft Libby Online (Natural) - English (United Kingdom)",
+    "Microsoft Maisie Online (Natural) - English (United Kingdom)",
+    "Microsoft Ryan Online (Natural) - English (United Kingdom)", // Ryan 具备“自动语言预测功能
+    "Microsoft Sonia Online (Natural) - English (United Kingdom)",
+    "Microsoft Thomas Online (Natural) - English (United Kingdom)",
+];
+
+var aExclude = [
+    "Microsoft Yunxia Online (Natural) - Chinese (Mainland)", // 中，男童声
+    "Microsoft Xiaoyi Online (Natural) - Chinese (Mainland)", // 中，女童声
+    "Microsoft Ana Online (Natural) - English (United States)", // 英，女童声
+];
+
+
